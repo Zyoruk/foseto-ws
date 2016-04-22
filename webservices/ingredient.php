1fd0dfaaf95a977218f70f306567c72ff53e7e9e
zyoruk <luisangel1993julio@gmail.com>
2016-04-22 11:19:07 -0600

    Added several functionality to the webservices. Sprint 1 and 2 . Missing database structure in order to have multiple recipes iin a single order.

--- webservices/ingredient.php
+++ webservices/ingredient.php
@@ -1,5 +1,5 @@
 <?php
-
+include 'connect_sql.php';
 class Ingredient {
 	function checkType($type) {
 		$query = "SELECT * FROM ingredients WHERE type =" . $type . ";";
@@ -8,9 +8,46 @@ class Ingredient {
 		if (! $result) {
 			die ( "{'error' : 'Error description:" . mysql_error ( $conn ) . " '}" );
 		}
-		
+		$result = mysql_fetch_assoc($result);
 		$result = json_encode ( $result );
 		echo $result;
+	}
+	
+	function deleteIngredient($ingid) {
+		$query = "DELETE FROM ingredients WHERE id = " . $ingid . "";
+		
+		if (! mysql_query ( $query )) {
+			die ( "{'error' : 'Error description:" . mysql_error ( $conn ) . " '}" );
+		}
+	}
+	
+	function editIngredient($ingid, $array) {
+		$query = "UPDATE ingredients SET";
+		$first = TRUE;
+		foreach ( $array as $key => $value ) {
+			if ($first) {
+				$query = $query . $key . "=" . $value;
+				$first = FALSE;
+			} else {
+				$query = $query . "," . $key . "=" . $value;
+			}
+		}
+		$query = $query . "WHERE id = " .$ingid . ";";
+		
+		if (! mysql_query ( $query )) {
+			die ( "{'error' : 'Error description:" . mysql_error ( $conn ) . " '}" );
+		}
+	}
+	
+	function showAvailableIngredients (){
+		$query = "SELECT id, name,status FROM ingredients WHERE status = N OR status = F; ";
+		$result = mysql_query ( $query );
+		
+		if (! $result) {
+			die ( "{'error' : 'Error description:" . mysql_error ( $conn ) . " '}" );
+		}
+		$result = mysql_fetch_assoc($result);
+		$result = json_encode ( $result );
 	}
 }
 
