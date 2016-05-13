acf7be9c1860f7413885a62f8951cbf987bdc023
zyoruk <luisangel1993julio@gmail.com>
2016-05-13 10:25:33 -0600

    Wrote the structure of the request class.

---
+++ webservices/request.php
@@ -0,0 +1,30 @@
+<?php
+include_once "connect_sql.php";
+
+/**
+ * This class contains many orders. In order to let the client add more orders once.
+ * 
+ */
+class Request{
+	
+	function createRequest(){
+		
+	}
+	
+	function deleteRequest($ird){
+		
+	}
+	
+	function removeOrder($rid , $oid){
+		
+	}
+	
+	function removeOrders($rid , $array){
+		
+	}
+	
+	function addOrder($rid , $oid ){
+		
+	}
+}
+?>
