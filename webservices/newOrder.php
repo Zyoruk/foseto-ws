682c7e80330ba0028574df9737e66340c9814db0
e3r8ick <eguicoro2@gmail.com>
2016-05-17 13:39:02 -0600

    Add files via upload

---
+++ webservices/newOrder.php
@@ -0,0 +1,33 @@
+<?php
+
+$servername = "localhost";
+$username = "root"; // add your mysql username
+$password = "erick"; // add your password
+$dbname = 'foseto';
+
+
+// Create connection
+$conn = mysql_connect ( $servername, $username, $password, TRUE );
+mysql_select_db ($dbname, $conn );
+
+$cont = "SELECT COUNT(id) FROM ingredients;";
+$num = mysql_query( $cont );
+$num = mysql_fetch_assoc($num);
+$num = $num["COUNT(id)"];
+$num =(int)$num;
+
+$final='[' ;
+for ($i=1; $i < $num; $i++) {
+  $query = "SELECT * FROM ingredients WHERE available = '1'  AND id ='".$i."';";
+  $result = mysql_query ( $query );
+  $result = mysql_fetch_assoc($result);
+  $result = json_encode($result);
+  $final = $final.$result.",";
+}
+$final = substr($final,0,-1);
+$final = $final."]";
+$final = json_encode($final);
+echo($final);
+file_put_contents('php://stderr',print_r(" final: ".$final."\n\n\n\n\n\n" ,TRUE));
+
+?>
