256897d74edf6e0c782550905cd118ebb45e7e92
e3r8ick <eguicoro2@gmail.com>
2016-05-24 16:53:53 -0600

    Add files via upload

--- webservices/currentOrders.php
+++ webservices/currentOrders.php
@@ -14,7 +14,7 @@ $conn = mysql_connect ( $servername, $username, $password, TRUE );
 mysql_select_db ($dbname, $conn );
 
 $final='[' ;
-$query = "SELECT * FROM orders INNER JOIN order_ingredient ON orders.clientId ='".$cid."' AND orders.id=order_ingredient.order_id INNER JOIN ingredients on ingredients.id=order_ingredient.ingredient_id;";
+$query = "SELECT * FROM orders INNER JOIN order_ingredient ON orders.clientId ='".$cid."' AND orders.id=order_ingredient.order_id INNER JOIN ingredients on ingredients.id=order_ingredient.ingredient_id WHERE orders.status ='P';";
 file_put_contents('php://stderr',print_r(" AAAAA: ".$query."\n\n\n\n\n\n" ,TRUE));
 $result = mysql_query ( $query );
 while($row = mysql_fetch_assoc($result)) {
@@ -25,6 +25,6 @@ $final = substr($final,0,-1);
 $final = $final."]";
 $final = json_encode($final);
 echo($final);
-//file_put_contents('php://stderr',print_r(" final: ".$final."\n\n\n\n\n\n" ,TRUE));
+file_put_contents('php://stderr',print_r(" final: ".$final."\n\n\n\n\n\n" ,TRUE));
 
 ?>
